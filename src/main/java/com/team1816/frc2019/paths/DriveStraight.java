66ae024dfb118abd76ae714397299995750ff859
Richik SC <sc.richik@gmail.com>
2019-09-18 11:13:03 -0500

    Optimize imports

--- src/main/java/com/team1816/frc2019/paths/DriveStraight.java
+++ src/main/java/com/team1816/frc2019/paths/DriveStraight.java
@@ -1,18 +1,12 @@
 package com.team1816.frc2019.paths;
 
-import com.team1816.frc2019.planners.DriveMotionPlanner;
 import com.team1816.lib.paths.PathBuilder;
 import com.team1816.lib.paths.PathContainer;
 import com.team254.lib.control.Path;
 import com.team254.lib.geometry.Pose2d;
-import com.team254.lib.geometry.Pose2dWithCurvature;
 import com.team254.lib.geometry.Rotation2d;
-import com.team254.lib.trajectory.Trajectory;
-import com.team254.lib.trajectory.timing.CentripetalAccelerationConstraint;
-import com.team254.lib.trajectory.timing.TimedState;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DriveStraight implements PathContainer {
