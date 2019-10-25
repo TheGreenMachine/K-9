157e081e1d988253d07c1b8bc840e29917b771c4
NickD <nikolad21889@isd273.org>
2019-10-24 20:14:26 -0500

    add logitech controller and abstract controller classes

--- src/main/java/com/team1816/lib/controlboard/LogitechController.java
+++ src/main/java/com/team1816/lib/controlboard/LogitechController.java
@@ -1,8 +1,6 @@
 package com.team1816.lib.controlboard;
 
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-
-public class XboxController extends Controller {
+public class LogitechController extends Controller {
 
     @Override
     protected int getLeftAxisId() {
@@ -11,15 +9,15 @@ public class XboxController extends Controller {
 
     @Override
     protected int getRightAxisId() {
-        return 4;
+        return 2;
     }
 
-    public XboxController(int port) {
+    public LogitechController(int port) {
         super(port);
     }
 
     public enum Button implements Controller.Button {
-        A(1), B(2), X(3), Y(4), LB(5), RB(6), BACK(7), START(8), L_JOYSTICK(9), R_JOYSTICK(10);
+        A(2), B(3), X(1), Y(4), LB(5), RB(6), BACK(9), START(10), L_JOYSTICK(11), R_JOYSTICK(12);
 
         public final int id;
 
@@ -35,11 +33,6 @@ public class XboxController extends Controller {
 
     @Override
     public double getTriggerScalar(Controller.Side side) {
-        return mController.getRawAxis(side == Side.LEFT ? 2 : 3);
+        return mController.getRawAxis(side == Side.LEFT ? 7 : 8);
     }
-
-    public void setRumble(boolean on) {
-        mController.setRumble(RumbleType.kRightRumble, on ? 1 : 0);
-    }
-
 }
